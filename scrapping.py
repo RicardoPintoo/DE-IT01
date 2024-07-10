74485e378304e1c72c3bcf9b2cbec96973c4ecb7
Ricardo Pinto <ricardoopintoo@hotmail.com>
2024-07-10 19:33:24 +0200

    scrapping price and district

--- scrapping.py
+++ scrapping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.domusbasto.com/imoveis/' 
+url = 'https://www.domusbasto.com' 
 
 params = {
     'bus': 1,  #'Venda'
@@ -12,9 +12,22 @@ params = {
 }
 
 # GET request
-response = requests.get(url, params=params)
+response = requests.get(url + '/imoveis/', params=params)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 properties = soup.find(id='PROPERTIES_LIST')
-print(properties.find_all(class_="fieldValue"))
+#print(properties.find_all(class_="fieldValue"))
+
+details_list = properties.find_all('span', id='details')
+# terreno = requests.get(url + details.a['href'])
+
+for details in details_list:
+    terreno_response = requests.get(url + details.a['href'])
+    soup_terreno = BeautifulSoup(terreno_response.content, 'html.parser')
+    price = soup_terreno.find('span', class_='value notranslate')
+    print(price.text)
+    detailRow = soup_terreno.find(class_='detailRow distrito')
+    district = detailRow.find(class_='value').text
+    print(district)
+    
 
